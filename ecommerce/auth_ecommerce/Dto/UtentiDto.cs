5c4c6199473618448cae3456cca47d5fa8481550
squar <dvdsqrc@gmail.com>
2025-03-14 15:22:46 +0100

    fixed Utenti and UtentiDto

--- ecommerce/auth_ecommerce/Dto/UtentiDto.cs
+++ ecommerce/auth_ecommerce/Dto/UtentiDto.cs
@@ -2,8 +2,8 @@
 {
     public class UtentiDto
     {
-        public string email { get; set; } = "";
-        public string password { get; set; } = "";
+        public string Username { get; set; } = "";
+        public string Password { get; set; } = "";
     }
 }
 
--- ecommerce/auth_ecommerce/Models/Utenti.cs
+++ ecommerce/auth_ecommerce/Models/Utenti.cs
@@ -3,7 +3,7 @@
     public class Utenti
     {
         public int Id { get; set; }
-        public string email { get; set; } = string.Empty("");
+        public string Username { get; set; } = string.Empty;
         public byte[] PasswordHash { get; set; } = Array.Empty<byte>();
         public byte[] PasswordSalt { get; set; } = Array.Empty<byte>();
     }
